629d92b889e1b7546671c22f01ba673c9e8461a6
Alyssa Hudon <piningpianist@gmail.com>
2016-04-19 23:10:44 -0700

    adding temporary jquery support

--- static/public/js/index.js
+++ static/public/js/index.js
@@ -1,1 +1,33 @@
 // index file
+
+'use strict';
+
+"use strict"
+
+$(function() {
+
+    
+    var email;
+    var password; 
+    
+    var loginForm = $('#login-form');
+    
+    // if the sign in button is clicked, validate fields and try to sign in 
+    $('#signin-button').click(function() {   
+        $('#otherError').hide();
+        
+        if (!loginForm.valid || loginForm.valid()) {    
+            email = $('#email').val();
+            password = $('#password').val();
+            $.post('/api/login', {email : email, password: password})
+                .done(function(data) {
+                    if (data.success) {
+                        window.location.replace('home.html');
+                    } else {                       
+                        $('#otherError').text(data.error);
+                        $('#otherError').show();
+                    }  
+                });
+        }
+    }); 
+}); 
