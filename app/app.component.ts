1ced84965099fb0a4ccd0af16d2ccc09d6addfdc
Alyssa Hudon <piningpianist@gmail.com>
2016-04-19 22:02:48 -0700

    adding typescript compilers and new packages for angular2, starting to scaffold out file structure for angular front end

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,1 +1,10 @@
 // placeholder app component file
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import { Component }
+
+@Component ({
+    selector: 'begreatful-app',
+    template: ,
+    directives: [RouterOutlet]
+})
