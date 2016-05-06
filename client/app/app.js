5d39ec3cfe8bdd4406833c7086c71e81dae1b856
northsal <northsal@uw.edu>
2016-05-06 10:49:24 -0700

    added SideNav, journal cards

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var beGreatfulApp = angular.module('beGreatfulApp', ['ngRoute','ngMaterial', 'loginController', 'accountController', 'entryController', 'userService']);
+var beGreatfulApp = angular.module('beGreatfulApp', ['ngRoute','ngMaterial', 'loginController', 'accountController', 'entryController', 'navController','cardController', 'userService']);
 
 
 beGreatfulApp.config(['$routeProvider', 
@@ -17,7 +17,21 @@ function($routeProvider) {
         when('/journal', {
             templateUrl: 'app/components/journal/journal.html'
         }).
+        when('/nav', {
+            templateUrl: 'app/components/SideNav/nav.html',
+            controller: 'NavCtrl'
+        }).
+        when('/card', {
+            templateUrl: 'app/components/journalcard/journalcard.html',
+            controller: 'CardCtrl'
+        }).
         otherwise({
             redirectTo: '/login'
         });   
 }]);
+
+beGreatfulApp.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('teal')
+    
+});
