cd8d88189e95965d6c4ac6296fbe194474edb21f
Alyssa Hudon <piningpianist@gmail.com>
2016-05-18 08:44:11 -0700

    updated journal list to use real data, adding validation to create account page

--- client/assets/js/util.js
+++ client/assets/js/util.js
@@ -1,9 +1,28 @@
 'use strict';
 
 function getEmoticon(identifier) {
-    if (identifier == 'happy') {
+    switch(identifier) {
+    case "good":
         return '../../assets/img/good.svg';
-    } else {
-        return '../../assets/img/good.svg'; 
+    case "great":
+        return '../../assets/img/great.svg';
+    case "okay":
+        return '../../assets/img/okay.svg';
+    case "poor":
+        return '../../assets/img/poor.svg';
+    case "awful":
+        return '../../assets/img/awful.svg';
+    default:
+        return '../../assets/img/good.svg';
     }
+    
+    // if (identifier == 'good') {
+    //     return '../../assets/img/good.svg';
+    // } else if (identifier == 'great') {
+    //     return '../../assets/img/good.'
+    // }
+    
+    // else {
+    //     return '../../assets/img/good.svg'; 
+    // }
 }
