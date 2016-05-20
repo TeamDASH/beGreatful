0e49c331967a7573fb221826820006f826664390
Alyssa Hudon <piningpianist@gmail.com>
2016-05-19 19:23:05 -0700

    adding server error messages to login page, adding 'create today's entry' card to the mobile journal board view

--- client/app/components/login/logincontroller.js
+++ client/app/components/login/logincontroller.js
@@ -5,12 +5,21 @@ var loginController = angular.module('loginController', []);
 loginController.controller('LoginCtrl', function ($scope, Login, $location) {
   $scope.user = new Login();
   
+  $scope.serverMessage = 'HELLO';
+  
   $scope.login = function(user) {
     Login.save(user, function(response) {
       console.log(response);
       if (response.success) {
+        $scope.serverMessage = "";
+        $scope.formLogin.$setValidity('specificError', true);
         $location.path('/journal')
-      } 
+      } else {
+        $scope.serverMessage = response.error;
+        $scope.formLogin.$setValidity('specificError', false);
+        console.log($scope.formLogin.$valid);
+        console.log($scope.serverMessage);
+      }
     });
   };
   
